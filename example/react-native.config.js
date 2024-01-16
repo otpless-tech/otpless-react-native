b9d3681ed32791f4daa6d051ebb2acf04ce9dbdc
Digvijay Singh <digvijay@otpless.com>
2024-01-16 17:38:13 +0530

    chore: ios build issue resolved

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -6,5 +6,6 @@ module.exports = {
     [pak.name]: {
       root: path.join(__dirname, '..'),
     },
+    ...(process.env.NO_FLIPPER ? { 'react-native-flipper': { platforms: { ios: null } } } : {})
   },
 };
